4cadddbf88e62d70f5b174ca45739a7f44cb21de
Jonathan Bachrach <jrb@pobox.com>
2001-10-29 23:00:59 +0000

    === 0.108 ===
    summary
    
      some general improvements for dynamic compilation including auto mode.
    
    adds
    
      (auto-eval) for turning on automatic dynamic compilation
    
    changes
    
      top comes up in auto mode detecting when dynamic compilation would
      be beneficial (by looking for <ast-method>'s in ast).
    
    fixes
    
      treatment of BOUNDP in dynamic compilation mode.
      printing of nul.

--- c/dlgrt.h
+++ c/dlgrt.h
@@ -3,11 +3,13 @@
 #undef VARREF
 #undef VARSET
 #undef FUNFOR
+#undef BOUNDP
 
-#define LOCNAM(x) x##V
-#define EXT(x, m, n)     static DLVAR_DAT LOCNAM(x) = { n, m, PNUL };
-#define DEF(x, m, n)     static P x = PNUL; static DLVAR_DAT LOCNAM(x) = { n, m, PNUL };
-#define VARREF(x)        Yast_evalYPbinding_value((P)&LOCNAM(x))
-#define VARSET(x, v)     Yast_evalYPbinding_value_setter((P)v, (P)&LOCNAM(x))
-#define FUNFOR(x)        extern P x##I(P, P)
+#define LOCNAM(x)    x##V
+#define EXT(x, m, n) static DLVAR_DAT LOCNAM(x) = { n, m, PNUL };
+#define DEF(x, m, n) static P x = PNUL; static DLVAR_DAT LOCNAM(x) = { n, m, PNUL };
+#define BOUNDP(x)    (P)Yast_evalYPbinding_boundQ((P)&LOCNAM(x))
+#define VARREF(x)    Yast_evalYPbinding_value((P)&LOCNAM(x))
+#define VARSET(x, v) Yast_evalYPbinding_value_setter((P)v, (P)&LOCNAM(x))
+#define FUNFOR(x)    extern P x##I(P, P)
 
