6a2b7dab752d7600f0ae9642107424c4987eaa8e
Andrew Sutherland <sombrero@alum.mit.edu>
2002-08-23 03:09:06 +0000

    Add the select() functionality.

--- src/goo/io/%net.c
+++ src/goo/io/%net.c
@@ -1,12 +1,15 @@
 #include "grt.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <stdio.h>
-#include <unistd.h>
 
 #define my_package YgooSioSnetYP
 
@@ -21,6 +24,11 @@
 #define MAXMSG 256
 
 #define GOOFUNC(name)    P YgooSioSnetYP ## name
+
+EXT(YgooSioSportYport_handle,"goo/io/port","port-handle");
+EXT(YgooSioSnetYsocket_eof,"goo/io/net","socket-eof");
+EXT(YgooStypesYlen, "goo/types", "len");
+EXT(YgooSmacrosYelt, "goo/macros", "elt");
 
 GOOFUNC(new_socket) ()
 {
@@ -83,7 +91,10 @@ GOOFUNC(listen_socket) (P sockfd, P iport)
       return YPfalse;
     }
 
-  listen(sockUnwrap(sockfd), 5);
+  if(listen(sockUnwrap(sockfd), 5) == 0)
+    return YPtrue;
+  else
+    return YPfalse;
 }
 
 GOOFUNC(accept_socket) (P sockfd)
@@ -122,28 +133,79 @@ GOOFUNC(socket_connected) (P sockfd)
     }
 }
 
-GOOFUNC(read_socket_byte) (P sockfd)
+GOOFUNC(read_max) (P sockfd)
 {
-  static char buf[1];
+  char buf[1024];
+  int retval;
 
-  if(recv(sockUnwrap(sockfd), &buf[0], 1, 0) < 1)
+  retval = recv(sockUnwrap(sockfd), &buf[0], 1024, 0);
+  if(retval == 0)
     {
-      return YPfalse;
+      return VARREF(YgooSioSnetYsocket_eof);
+    }
+  else if(retval < 0)
+    {
+      if(errno == EAGAIN)
+	{
+	  return YPfalse;
+	}
+      else
+	{
+	  return VARREF(YgooSioSnetYsocket_eof);
+	}
     }
   
-  return YPcb(buf[0]);
+  return YPsb_with_len(buf, retval);
+}
+
+GOOFUNC(read_socket_byte) (P sockfd)
+{
+  char buf[1];
+  int retval;
+
+  retval = recv(sockUnwrap(sockfd), &buf[0], 1, 0);
+  if(retval == 0)
+    {
+      return VARREF(YgooSioSnetYsocket_eof);
+    }
+  else if(retval < 0)
+    {
+      if(errno == EAGAIN)
+	{
+	  return YPfalse;
+	}
+      else
+	{
+	  return VARREF(YgooSioSnetYsocket_eof);
+	}
+    }
+  
+  return YPcb((P)((int)buf[0]));
 }
 
 GOOFUNC(peek_socket_byte) (P sockfd)
 {
   char buf[1];
+  int retval;
 
-  if(recv(sockUnwrap(sockfd), &buf[0], 1, MSG_PEEK) < 1)
+  retval = recv(sockUnwrap(sockfd), &buf[0], 1, MSG_PEEK);
+  if(retval == 0)
     {
-      return YPfalse;
+      return VARREF(YgooSioSnetYsocket_eof);
+    }
+  else if(retval < 0)
+    {
+      if(errno == EAGAIN)
+	{
+	  return YPfalse;
+	}
+      else
+	{
+	  return VARREF(YgooSioSnetYsocket_eof);
+	}
     }
   
-  return YPcb(buf[0]);
+  return YPcb((P)((int)buf[0]));
 }
 
 GOOFUNC(write_socket_byte) (P sockfd, P byte)
@@ -168,8 +230,9 @@ GOOFUNC(write_socket_string) (P sockfd, P str)
   int len;
 
   dastr = (char *)YPsu(str);
-  len = strlen(dastr);
+  len = (int)YPslen(str);
 
+  // I have a feeling that this should be looping...
   if(send(sockUnwrap(sockfd), dastr, len, 0) < len)
     {
       return YPfalse;
@@ -185,5 +248,208 @@ GOOFUNC(close_socket) (P sockfd)
   close(sockUnwrap(sockfd));
 
   return YPtrue;
+}
+
+GOOFUNC(make_non_blocking) (P sockfd)
+{
+  int opts;
+  int sock = sockUnwrap(sockfd);
+
+  opts = fcntl(sock, F_GETFL);
+  if(opts < 0)
+    {
+      return YPfalse;
+    }
+
+  opts |= O_NONBLOCK;
+
+  if(fcntl(sock, F_SETFL, opts) < 0)
+    {
+      return YPfalse;
+    }
+
+  return YPtrue;
+}
+
+#define YPtlen(a)        (int)YPiu(XCALL1(1, VARREF(YgooStypesYlen), a))
+#define YPtelt(a, b)     XCALL2(1, VARREF(YgooSmacrosYelt), a, YPib((P)b))
+
+GOOFUNC(select) (P readvec, P writevec, P exceptvec, P timeout)
+{
+  fd_set readset, writeset, exceptset;
+  P read_outvec, write_outvec, except_outvec, result_tup;
+  int highest=0;
+  struct timeval tv;
+  int i, len, time_millis, retval;
+
+  DEFCREGS();
+
+  FD_ZERO(&readset);
+  FD_ZERO(&writeset);
+  FD_ZERO(&exceptset);
+
+  // loop over read
+  if((readvec != YPfalse) && ((len = (int)YPtlen(readvec)) > 0))
+    {
+      for(i=0; i < len; i++)
+	{
+	  int fd = sockUnwrap(XCALL1(1, VARREF(YgooSioSportYport_handle), YPtelt(readvec, i)));
+	  FD_SET(fd, &readset);
+
+	  if(fd > highest)
+	    highest=fd;
+	}
+    }
+  
+  // loop over write
+  if((writevec != YPfalse) && ((len = (int)YPtlen(writevec)) > 0))
+    {
+      for(i=0; i < len; i++)
+	{
+	  int fd = sockUnwrap(XCALL1(1, VARREF(YgooSioSportYport_handle), YPtelt(writevec, i)));
+	  FD_SET(fd, &writeset);
+
+	  if(fd > highest)
+	    highest=fd;
+	}
+    }
+   
+  // loop over except
+  if((exceptvec != YPfalse) && ((len = (int)YPtlen(exceptvec)) > 0))
+    {
+      for(i=0; i < len; i++)
+	{
+	  int fd = sockUnwrap(XCALL1(1, VARREF(YgooSioSportYport_handle), YPtelt(exceptvec, i)));
+	  FD_SET(fd, &exceptset);
+
+	  if(fd > highest)
+	    highest=fd;
+	}
+    }
+  
+
+  if(timeout != YPfalse)
+    {
+      time_millis = intUnwrap(timeout);
+      tv.tv_sec = time_millis / 1000;
+      tv.tv_usec = (time_millis % 1000) * 1000;
+      
+      retval = select(highest+1, &readset, &writeset, &exceptset, &tv);
+    }
+  else
+    {
+      retval = select(highest+1, &readset, &writeset, &exceptset, NULL);
+    }
+  if(retval < 0)
+    {
+      printf("Something's real bad wrong... in my pants!\n");
+      return YPfalse;
+    }
+  
+  // loop over read
+  if((readvec != YPfalse) && ((len = (int)YPtlen(readvec)) > 0))
+    {
+      int numSet = 0, idxTup=0;
+
+      // Find how many there are in this guy.
+      for(i=0; i < len; i++)
+	{
+	  int fd = sockUnwrap(XCALL1(1, VARREF(YgooSioSportYport_handle), YPtelt(readvec, i)));
+	  if(FD_ISSET(fd, &readset))
+	    numSet++;
+	}
+      
+      // Create a tup and fill it up.  
+      // Note the inefficiency with which I punish the infidels who would dare attempt 
+      //  to do things in a non-blocking fashion!  They will pay!!!
+      read_outvec = YPPtfab((P)numSet, (P)0);
+
+      for(i=0; i < len; i++)
+	{
+	  P obj_val = YPtelt(readvec, i);
+	  int fd = sockUnwrap(XCALL1(1, VARREF(YgooSioSportYport_handle), obj_val));
+	  if(FD_ISSET(fd, &readset))
+	    {
+	      YPtelt_setter(obj_val, read_outvec, idxTup++);
+	    }
+	}
+    }
+  else
+    {
+      read_outvec = YPfalse;
+    }
+
+  // loop over write
+  if((writevec != YPfalse) && ((len = (int)YPtlen(writevec)) > 0))
+    {
+      int numSet = 0, idxTup=0;
+
+      // Find how many there are in this guy.
+      for(i=0; i < len; i++)
+	{
+	  int fd = sockUnwrap(XCALL1(1, VARREF(YgooSioSportYport_handle), YPtelt(writevec, i)));
+	  if(FD_ISSET(fd, &writeset))
+	    numSet++;
+	}
+      
+      // Create a tup and fill it up.  
+      // Note the inefficiency with which I punish the infidels who would dare attempt 
+      //  to do things in a non-blocking fashion!  They will pay!!!
+      write_outvec = YPPtfab((P)numSet, (P)0);
+
+      for(i=0; i < len; i++)
+	{
+	  P obj_val = YPtelt(writevec, i);
+	  int fd = sockUnwrap(XCALL1(1, VARREF(YgooSioSportYport_handle), obj_val));
+	  if(FD_ISSET(fd, &writeset))
+	    {
+	      YPtelt_setter(obj_val, write_outvec, idxTup++);
+	    }
+	}
+    }
+  else
+    {
+      write_outvec = YPfalse;
+    }
+
+  // loop over except
+  if((exceptvec != YPfalse) && ((len = (int)YPtlen(exceptvec)) > 0))
+    {
+      int numSet = 0, idxTup=0;
+
+      // Find how many there are in this guy.
+      for(i=0; i < len; i++)
+	{
+	  int fd = sockUnwrap(XCALL1(1, VARREF(YgooSioSportYport_handle), YPtelt(exceptvec, i)));
+	  if(FD_ISSET(fd, &exceptset))
+	    numSet++;
+	}
+      
+      // Create a tup and fill it up.  
+      // Note the inefficiency with which I punish the infidels who would dare attempt 
+      //  to do things in a non-blocking fashion!  They will pay!!!
+      except_outvec = YPPtfab((P)numSet, (P)0);
+
+      for(i=0; i < len; i++)
+	{
+	  P obj_val = YPtelt(exceptvec, i);
+	  int fd = sockUnwrap(XCALL1(1, VARREF(YgooSioSportYport_handle), obj_val));
+	  if(FD_ISSET(fd, &exceptset))
+	    {
+	      YPtelt_setter(obj_val, except_outvec, idxTup++);
+	    }
+	}
+    }
+  else
+    {
+      except_outvec = YPfalse;
+    }
+
+  result_tup = YPPtfab(3, (P)0);
+  YPtelt_setter(read_outvec, result_tup, 0);
+  YPtelt_setter(write_outvec, result_tup, 1);
+  YPtelt_setter(except_outvec, result_tup, 2);
+
+  return result_tup;
 }
 
